07b0609aeadcf1fea2620c667e4a2af97872d21b
Shreyansh Jain <m.shreyansh.jain@gmail.com>
2024-04-16 06:51:09 +0530

    minor change to user route

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -2,31 +2,31 @@ import { NextRequest, NextResponse } from "next/server";
 import { UserResponseType } from "./_helpers/types";
 
 export async function GET(
-  req: NextRequest
+    req: NextRequest
 ): Promise<NextResponse<UserResponseType>> {
-  const verifyToken = req.cookies.get("verifyToken")?.value;
+    const verifyToken = req.cookies.get("token")?.value;
 
-  if (!verifyToken) {
+    if (!verifyToken) {
+        return NextResponse.json({
+            message: "Invalid verify token",
+        });
+    }
+
+    if (false) {
+        return NextResponse.json({
+            message: "User not found",
+        });
+    }
+
     return NextResponse.json({
-      message: "Invalid verify token",
+        message: "User info fetched",
+        data: {
+            user: {
+                email: "sdfsdf",
+                password: "sdfsdf",
+                username: "sdfsdf",
+                _id: "sdfsdf",
+            },
+        },
     });
-  }
-
-  // find user by verifyToken
-  if (false) {
-    return NextResponse.json({
-      message: "User not found",
-    });
-  }
-
-  return NextResponse.json({
-    message: "User info fetched",
-    data: {
-      user: {
-        email: "sdfsdf",
-        password: "sdfsdf",
-        username: "sdfsdf",
-      },
-    },
-  });
 }
